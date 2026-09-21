9760703ab49e5c6282c9fd1f5f1ffe7a286f7ce0
Dharizza Espinach <dharizza@gmail.com>
2026-09-21 11:33:12 -0600

    Added smoke wiring test.

---
+++ web/modules/custom/campus_greeting/tests/src/Kernel/SmokeTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Drupal\Tests\campus_greeting\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * @group campus_greeting
+ */
+class SmokeTest extends KernelTestBase {
+  public function testWiringWorks(): void {
+    $this->assertTrue(TRUE);
+  }
+}
